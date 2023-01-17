69eb77311775cd607bd19d9d2da69eb7a117bb3e
Samuel Leutner Ferreira <samuel.leutner@hotmail.com>
2023-01-17 00:32:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const filterElement = document.querySelector('header input')
 const cards = document.querySelectorAll('.cards li')
+const paragraphs = document.querySelectorAll('.cards p')
 
 filterElement.addEventListener('input', filterCards)
 
@@ -7,9 +8,11 @@ function filterCards () {
     if(filterElement.value != ''){
         for (let card of cards){
             let title = card.querySelector('h2')
+            let paragraph = card.querySelector('p')
             title = title.textContent.toLowerCase()
+            paragraph = paragraph.textContent.toLowerCase()
             let filterText = filterElement.value.toLowerCase()
-            if(!title.includes(filterText)) {
+            if(!title.includes(filterText) && !paragraph.includes(filterText)) {
                 card.style.display = "none"
             }
             else {
@@ -22,3 +25,4 @@ function filterCards () {
         }
     }
 }
+
